518e01976e110031eaaaf991e556137176262b22
Lynx-Sh1n0b1 <40631208+Lynx-Sh1n0b1@users.noreply.github.com>
2023-04-07 17:26:13 +0300

    Update curl-urls-from-txt.ps1

--- curl-urls-from-txt.ps1
+++ curl-urls-from-txt.ps1
@@ -1,28 +1,16 @@
 clear
 
-# Delete the existing output file if it exists
-$output_file = "curled.txt"
-if (Test-Path $output_file) {
-    Remove-Item $output_file
-}
-
-# Create banner$banner = @
-" ___________________________________________________________
+# Create banner
+$banner = 
+"___________________________________________________________
 |                                                           |
 |    Give me txt file with URLS and LET'S CURL THEM!        |
 |                                                           |
 |___________________________________________________________| 
 "
-# Output banner 
 Write-Output $banner
 # Prompt for text file with URLs
 $file = Read-Host "Enter path to text file with URLs"
-
-# Check if the file exists
-if (-not (Test-Path $file_path)) {
-Write-Host "The file doesn't exist."
-exit
-}
 # Read URLs from file
 $urls = Get-Content $file
 # Create output file
@@ -33,14 +21,17 @@ foreach ($url in $urls) {
 # Output URL being curled
 Write-Host "Curling: $url"
 # Send curl request and output response to file
-$curlOutput = curl $url
+$curlOutput = Invoke-WebRequest $url
+Add-Content $outputFile ""
 Add-Content $outputFile "URL: $url"
+Add-Content $outputFile ""
+Add-Content $outputFile ""
 Add-Content $outputFile $curlOutput.Content
 Add-Content $outputFile "___________________________________________________________________"
 }
 # Output path to output file
 Write-Host "Done!"
-Write-Host "Curl output saved to: $outputFile"
+Write-Host "Curl output saved to: $outputFile  <-------------------"
 Write-Host "___________________________________________________________________"
 # Show the contents of the output file
 cat $output_file
